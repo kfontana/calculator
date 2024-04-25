95b95b04744de528cb510f4b18902d8410ee59aa
Kevin <kevinfontana1993@gmail.com>
2024-04-25 12:57:10 -0300

    Add function operate

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let firstNumber;
 let secondNumber;
 let operator;
 
-function add(num1, num2) {
-  return num1 + num2;
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 }
 
 function substract(num1, num2) {
@@ -16,4 +16,16 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+function operate(firstNumber, secondNumber, operator) {
+  if (operator === "add") {
+    return add(firstNumber, secondNumber);
+  } else if (operator === "substract") {
+    return substract(firstNumber, secondNumber);
+  } else if (operator === "multiply") {
+    return multiply(firstNumber, secondNumber);
+  } else if (operator === "divide") {
+    return divide(firstNumber, secondNumber);
+  }
 }
