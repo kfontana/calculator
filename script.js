20860a72ce09378bf0cf8b9ca9dff7ac06b3e43f
Kevin <kevinfontana1993@gmail.com>
2024-04-27 16:47:09 -0300

    Add basic logic to the calculator

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let firstNumber;
 let secondNumber;
+let firstValue = [];
+let secondValue = [];
+let operatorFlag = false;
 let operator;
-let displayValue;
 
 const btnOne = document.querySelector("#btn-1");
 btnOne.addEventListener("click", displayNumberOne);
@@ -35,71 +37,227 @@ btnZero.addEventListener("click", displayNumberZero);
 
 const display = document.querySelector("#display");
 
+const addBtn = document.querySelector("#add");
+addBtn.addEventListener("click", operatorFlagAdd);
+
+const subBtn = document.querySelector("#substract");
+subBtn.addEventListener("click", operatorFlagSub);
+
+const multiplyBtn = document.querySelector("#multiply");
+multiplyBtn.addEventListener("click", operatorFlagMultiply);
+
+const divideBtn = document.querySelector("#divide");
+divideBtn.addEventListener("click", operatorFlagDivide);
+
+const operateBtn = document.querySelector("#operate");
+operateBtn.addEventListener("click", operate);
+
+const clearBtn = document.querySelector("#clr-btn");
+clearBtn.addEventListener("click", clear);
+
+function clear() {
+  operatorFlag = false;
+  display.textContent = "";
+  firstValue = [];
+  secondValue = [];
+}
+
+function operatorFlagAdd() {
+  operatorFlag = true;
+  operator = "add";
+  display.textContent = "";
+}
+
+function operatorFlagSub() {
+  operatorFlag = true;
+  operator = "substract";
+  display.textContent = "";
+}
+
+function operatorFlagMultiply() {
+  operatorFlag = true;
+  operator = "multiply";
+  display.textContent = "";
+}
+
+function operatorFlagDivide() {
+  operatorFlag = true;
+  operator = "divide";
+  display.textContent = "";
+}
+
 function displayNumberOne() {
-  displayValue = display.textContent = 1;
-  console.log(displayValue);
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 1));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 1));
+    }
+    display.textContent = secondValue.join("");
+  }
 }
 
 function displayNumberTwo() {
-  displayValue = display.textContent = 2;
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 2));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 2));
+    }
+    display.textContent = secondValue.join("");
+  }
 }
 
 function displayNumberThree() {
-  displayValue = display.textContent = 3;
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 3));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 3));
+    }
+    display.textContent = secondValue.join("");
+  }
 }
 
 function displayNumberFour() {
-  displayValue = display.textContent = 4;
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 4));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 4));
+    }
+    display.textContent = secondValue.join("");
+  }
 }
 
 function displayNumberFive() {
-  displayValue = display.textContent = 5;
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 5));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 5));
+    }
+    display.textContent = secondValue.join("");
+  }
 }
 
 function displayNumberSix() {
-  displayValue = display.textContent = 6;
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 6));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 6));
+    }
+    display.textContent = secondValue.join("");
+  }
 }
 
 function displayNumberSeven() {
-  displayValue = display.textContent = 7;
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 7));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 7));
+    }
+    display.textContent = secondValue.join("");
+  }
 }
 
 function displayNumberEight() {
-  displayValue = display.textContent = 8;
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 8));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 8));
+    }
+    display.textContent = secondValue.join("");
+  }
 }
 
 function displayNumberNine() {
-  displayValue = display.textContent = 9;
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 9));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 9));
+    }
+    display.textContent = secondValue.join("");
+  }
 }
 
 function displayNumberZero() {
-  displayValue = display.textContent = 0;
-}
-
-function add(firstNumber, secondNumber) {
-  return firstNumber + secondNumber;
-}
-
-function substract(num1, num2) {
-  return num1 - num2;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
-function operate(firstNumber, secondNumber, operator) {
-  if (operator === "add") {
-    return add(firstNumber, secondNumber);
-  } else if (operator === "substract") {
-    return substract(firstNumber, secondNumber);
-  } else if (operator === "multiply") {
-    return multiply(firstNumber, secondNumber);
-  } else if (operator === "divide") {
-    return divide(firstNumber, secondNumber);
+  if (operatorFlag === false) {
+    if (firstValue.length <= 8) {
+      firstValue.push((display.textContent = 0));
+    }
+    display.textContent = firstValue.join("");
+  } else {
+    if (secondValue.length <= 8) {
+      secondValue.push((display.textContent = 0));
+    }
+    display.textContent = secondValue.join("");
   }
+}
+
+function operate() {
+  if (operator === "add") {
+    return add(firstValue, secondValue);
+  } else if (operator === "substract") {
+    return substract(firstValue, secondValue);
+  } else if (operator === "multiply") {
+    return multiply(firstValue, secondValue);
+  } else if (operator === "divide") {
+    return divide(firstValue, secondValue);
+  }
+
+  function add(firstValue, secondValue) {
+    firstValue = parseInt(firstValue.join(""));
+    secondValue = parseInt(secondValue.join(""));
+    return (display.textContent = firstValue + secondValue);
+  }
+}
+
+function substract(firstValue, secondValue) {
+  firstValue = parseInt(firstValue.join(""));
+  secondValue = parseInt(secondValue.join(""));
+  return (display.textContent = firstValue - secondValue);
+}
+
+function multiply(firstValue, secondValue) {
+  firstValue = parseInt(firstValue.join(""));
+  secondValue = parseInt(secondValue.join(""));
+  return (display.textContent = firstValue * secondValue);
+}
+
+function divide(firstValue, secondValue) {
+  firstValue = parseInt(firstValue.join(""));
+  secondValue = parseInt(secondValue.join(""));
+  return (display.textContent = firstValue / secondValue);
 }
